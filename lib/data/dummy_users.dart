c7d7c0741054a5ad8c04dd115986f1361d851b66
bigMARAC <marcos.length@gmail.com>
2021-02-27 18:45:39 -0300

    Initial commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:helloworld/models/User.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Marcos Emanuel',
+      email: 'marcos.length@gmail.com',
+      avatar:
+          'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Júlio Bianco',
+      email: 'jujusbianco@gmail.com',
+      avatar:
+          'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Cauê Augusto',
+      email: 'caueimaboss@gmail.com',
+      avatar:
+          'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_1280.png')
+};
